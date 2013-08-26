126881e876b1a0e0d8d6c7250d99df60a9319b81
caruana <caruanab@gmail.com>
2013-08-26 19:08:41 +0000

    all

---
+++ ADevsLife.BingGoogle.Web/Search.svc.cs
@@ -0,0 +1,25 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using ADevsLife.BingGoogle.SearchAgent;
+
+namespace ADevsLife.BingGoogle.Web
+{
+    [ServiceContract(Namespace = "ADevsLife")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class Search
+    {
+        [OperationContract]
+        [WebGet]
+        public CombinedSearchResults DoSearch(string q)
+        {
+            if (!string.IsNullOrWhiteSpace(q))
+            {
+                var search = new CallSearchEngine();
+                return search.Search(q);
+            }
+            return null;
+        }
+
+    }
+}
